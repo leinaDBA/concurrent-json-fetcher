870174683cdc880f7e94118c02d4aaed86cf682d
Daniel <d.lee.2@elsevier.com>
2021-02-09 17:23:50 +0000

    add tests for the bin script

---
+++ bin/index.test.js
@@ -0,0 +1,45 @@
+import chai, { expect } from 'chai';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+describe('bin script', () => {
+  const returnedData = 'done';
+  let requestJSONsStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+    requestJSONsStub = sinon.stub().resolves(returnedData);
+    process.argv[2] = undefined;
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  const runBinWithArgs = (args) => {
+    process.argv[2] = args;
+    proxyquire('./index.js', {
+      '../dist/index.js': { default: requestJSONsStub },
+    });
+  };
+
+  it('splits URLs', () => {
+    const urls = 'first;second;third';
+
+    runBinWithArgs(urls);
+
+    expect(requestJSONsStub).to.have.been.calledOnceWithExactly(
+      urls.split(';')
+    );
+  });
+
+  it('prints the results', async () => {
+    await runBinWithArgs('foo');
+
+    expect(consoleLogSpy).to.have.been.calledOnceWithExactly(returnedData);
+  });
+});
---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+describe('requestJSONs', () => {
+  beforeEach(() => {});
+
+  describe('validation', () => {
+    it('validates arguments are the correct shape', () => {});
+    it('validates arguments are URLs', () => {});
+  });
+
+  it('returns results in the correct shape', () => {});
+});
